49734f36483361cfa8212d5329911e27407e20e4
Maciej Uberna <maciej.uberna@gmail.com>
2020-01-29 19:36:36 +0100

    Lesson195 "Making a Post Selectable" added postSelectedHandler and passed it as cliked in <post> render tag.

--- blog-http-requests-test/src/containers/Blog/Blog.js
+++ blog-http-requests-test/src/containers/Blog/Blog.js
@@ -9,7 +9,8 @@ import './Blog.css';
 class Blog extends Component {
 
     state = {
-        posts: []
+        posts: [],
+        selectedPostId: null
     };
 
     componentDidMount() {
@@ -28,11 +29,20 @@ class Blog extends Component {
             });
     }
 
+    postSelectedHandler = (id) => {
+        this.setState({selectedPostId: id});
+    }
+
     render () {
         //console.log(this.state)
         if(this.state.posts) {
             const posts = this.state.posts.map(post => {
-                return <Post key={post.id} title={post.title} author={post.author}/>
+                return <Post 
+                key={post.id} 
+                title={post.title} 
+                author={post.author}
+                clicked={() => this.postSelectedHandler(post.id)}
+                />
             });
             return (
                 <div>
@@ -40,7 +50,7 @@ class Blog extends Component {
                         {posts}
                     </section>
                     <section>
-                        <FullPost />
+                        <FullPost id={this.state.selectedPostId}/>
                     </section>
                     <section>
                         <NewPost />
