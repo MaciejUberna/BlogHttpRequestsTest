5af0fc1d863b91cbb2ba1f481e34014e2c649965
Maciej Uberna <maciej.uberna@gmail.com>
2020-02-06 21:25:30 +0100

    Lesson225 "Switching Between Pages" in Blog.js is implemented but we use links and each time one is clicked page reloads entirelly.

--- blog-http-requests-test/src/containers/Blog/Blog.js
+++ blog-http-requests-test/src/containers/Blog/Blog.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 //import axios from 'axios';
 import Posts from './Posts/Posts';
+import NewPost from './NewPost/NewPost';
+
 import './Blog.css';
 
 import { Route } from 'react-router-dom';
@@ -22,6 +24,8 @@ class Blog extends Component {
                     {/* <Route path="/" exact render={() => <h1>Home</h1>}/>
                     <Route path="/" render={() => <h1>Home 2</h1>}/> */}
                     <Route path="/" exact component={Posts} />
+                    <Route path="/new-post" component={NewPost} />
+                    {/* <Route path */}
                 </div>
             );
         }
