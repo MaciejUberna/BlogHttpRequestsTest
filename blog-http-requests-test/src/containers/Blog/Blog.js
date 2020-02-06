202d203f05541f81f7e1808420a902a1043170c7
Maciej Uberna <maciej.uberna@gmail.com>
2020-02-06 21:15:00 +0100

    Lesson223 "Setting up and Rendering Routes" added Route functionality to Blog.js.

--- blog-http-requests-test/src/containers/Blog/Blog.js
+++ blog-http-requests-test/src/containers/Blog/Blog.js
@@ -3,6 +3,8 @@ import React, { Component } from 'react';
 import Posts from './Posts/Posts';
 import './Blog.css';
 
+import { Route } from 'react-router-dom';
+
 class Blog extends Component {
 
     render () {
@@ -13,11 +15,12 @@ class Blog extends Component {
                         <nav>
                             <ul>
                                 <li><a href="/">Home</a></li>
-                                <li><a href="/">New Post</a></li>
+                                <li><a href="/new-post">New Post</a></li>
                             </ul>
                         </nav>
                     </header>
-                    <Posts />
+                    <Route path="/" exact render={() => <h1>Home</h1>}/>
+                    <Route path="/" render={() => <h1>Home 2</h1>}/>
                 </div>
             );
         }
