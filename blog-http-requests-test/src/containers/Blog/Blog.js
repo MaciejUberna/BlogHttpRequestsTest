00644d2a18566e7a32a92c13ab6757b97f89a13e
Maciej Uberna <maciej.uberna@gmail.com>
2020-02-06 21:55:04 +0100

    Lesson226 "Using Links to Switch Pages" added Link tags to Blog.js.

--- blog-http-requests-test/src/containers/Blog/Blog.js
+++ blog-http-requests-test/src/containers/Blog/Blog.js
@@ -5,7 +5,7 @@ import NewPost from './NewPost/NewPost';
 
 import './Blog.css';
 
-import { Route } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
 
 class Blog extends Component {
 
@@ -16,8 +16,12 @@ class Blog extends Component {
                     <header>
                         <nav>
                             <ul>
-                                <li><a href="/">Home</a></li>
-                                <li><a href="/new-post">New Post</a></li>
+                                <li><Link to="/">Home</Link></li>
+                                <li><Link to={{
+                                    pathname: '/new-post',
+                                    hash: '#submit',
+                                    search: '?quick-submit=true'
+                                }}>New Post</Link></li>
                             </ul>
                         </nav>
                     </header>
@@ -25,7 +29,6 @@ class Blog extends Component {
                     <Route path="/" render={() => <h1>Home 2</h1>}/> */}
                     <Route path="/" exact component={Posts} />
                     <Route path="/new-post" component={NewPost} />
-                    {/* <Route path */}
                 </div>
             );
         }
