e656d7876c7127502aa1a72f947b7f7a7b12835a
Maciej Uberna <maciej.uberna@gmail.com>
2020-02-10 23:05:29 +0100

    In Posts.js history changed to simple string.

--- blog-http-requests-test/src/containers/Blog/Posts/Posts.js
+++ blog-http-requests-test/src/containers/Blog/Posts/Posts.js
@@ -36,8 +36,8 @@ class Posts extends Component {
 
     postSelectedHandler = (id) => {
         // "pathname" is the encoded param. 
-        this.props.history.push({pathname: '/posts/' + id});
-        // this.props.history.push({'/posts/' + id});
+        //this.props.history.push({pathname: '/posts/' + id});
+        this.props.history.push('/posts/' + id);
     }
 
     render () {
