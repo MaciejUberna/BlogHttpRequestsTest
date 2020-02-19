6cd1f73406c4f18ee6fc3d7c0f91772695fff52e
Maciej Uberna <maciej.uberna@gmail.com>
2020-02-19 23:21:22 +0100

    Assigment1-5 Installed query-string in order to parse search queries from url.

--- routing-assignment-problem/src/containers/Course/Course.js
+++ routing-assignment-problem/src/containers/Course/Course.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
+//package to parse queries
+import queryString from 'query-string';
 
 class Course extends Component {
+
+    state = {
+        id: null,
+        title: null
+    }
+
+    componentDidMount() {
+        //console.log('this.props.location.search: ',this.props.location.search);
+        const queries = queryString.parse(this.props.location.search);
+        //console.log('queries: ',queries.title);
+        this.setState(
+            {id: this.props.match.params.id,
+            title: queries.title}
+        );
+    }
+
     render () {
         return (
             <div>
-                <h1>_COURSE_TITLE_</h1>
-                <p>You selected the Course with ID: {this.props.id}</p>
+                <h1>{this.state.title}</h1>
+                {/* {console.log("Course props log:",this.props)} */}
+                <p>You selected the Course with ID: {this.state.id} </p>
             </div>
         );
     }
--- routing-assignment-problem/src/containers/Courses/Courses.js
+++ routing-assignment-problem/src/containers/Courses/Courses.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import {NavLink} from 'react-router-dom';
+import {NavLink, Route} from 'react-router-dom';
+import Course from '../Course/Course';
 
 import classes from './Courses.module.css';
 
@@ -20,13 +21,14 @@ class Courses extends Component {
                     {
                         this.state.courses.map( course => {
                             return <article className={classes.Course} key={course.id}>
-                                <NavLink activeClassName={classes.Link} to={"/courses/"+course.id}>
+                                <NavLink activeClassName={classes.Link} to={"/courses/"+course.id+'?title='+course.title}>
                                     {course.title}
                                 </NavLink>
                             </article>;
                         } )
                     }
                 </section>
+                <Route path={this.props.match.url + '/:id'} exact component={Course} />
             </div>
         );
     }
